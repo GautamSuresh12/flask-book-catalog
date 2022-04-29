9f8005780c89da7a6517a7a2c1cd2c7903a9dab4
GautamSuresh12 <rssgautam157@gmail.com>
2022-04-29 22:05:54 +0530

    flask-advanced

---
+++ config/dev.py
@@ -0,0 +1,4 @@
+DEBUG = True
+SECRET_KEY = 'sureshpriya'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sureshpriya@localhost:5433/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ config/prod.py
@@ -0,0 +1,6 @@
+import os
+
+DEBUG = False
+SECRET_KEY = 'sureshpriya'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sureshpriya@localhost:5433/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
